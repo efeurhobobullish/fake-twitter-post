e58cc9725c08f4247319774a08a3a5251f87f4e9
Empire Tech <151479661+efeurhobobullish@users.noreply.github.com>
2025-08-12 01:01:38 +0100

    Update app.py

--- app.py
+++ app.py
@@ -35,5 +35,7 @@ def index():
         }
     return render_template('index.html', tweet=tweet)
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
